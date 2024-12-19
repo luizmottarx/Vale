9d2aa90a28cc652f006fa077496cec0a1eb2fb14
luizmottarx <luizdias516@gmail.com>
2024-12-19 15:57:06 -0300

    -

--- teste1.py
+++ teste1.py
@@ -1,10 +1,13 @@
 # teste1.py
 
 import os
+from testeBD import DatabaseManager
 
 class FileProcessor:
     def __init__(self, directory):
         self.directory = directory
+        self.db_manager = DatabaseManager()
+        self.metadados_map = self.db_manager.get_metadados_map()
 
     def process_gds_file(self, gds_file):
         try:
@@ -15,9 +18,14 @@ class FileProcessor:
             for line in lines[:57]:
                 parts = line.strip().split(',')
                 if len(parts) == 2 and parts[1].strip():
-                    chave = parts[0].strip().strip('"')
+                    chave_completa = parts[0].strip().strip('"')
                     valor = parts[1].strip().strip('"')
-                    metadados[chave] = valor
+                    abreviacao = self.metadados_map.get(chave_completa)
+                    if abreviacao:
+                        metadados[abreviacao] = valor
+                    else:
+                        # Se não houver mapeamento, manter a chave original
+                        metadados[chave_completa] = valor
 
             return metadados
 
